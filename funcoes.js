ab5508a2104dbbefa5d76341be8a0592f5338e81
NotFelBRo <tagm554@gmail.com>
2026-05-19 19:59:55 -0300

    Função de aparecer o personagem na página

--- funcoes.js
+++ funcoes.js
@@ -4,3 +4,28 @@ async function buscarPersonagem() {
   const personagens = await response.json();
   console.log(personagens);
 }
+
+// Aqui, iniciei uma função em que todos os personagens da lista aparecerem no quadro da página e as suas descrição.
+buscarPersonagem();
+
+let personagens = [];
+let indexAtual = 0;
+
+//listei todos os persomagems
+async function carregarPersonagens() {
+  const response = await fetch("https://hp-api.onrender.com/api/characters");
+  const personagens = await response.json();
+}
+
+// as imagens e descrições aparecem na página
+function mostrarPersonagem(index) {
+  const p = personagens[index];
+  if (!p) return;
+
+  // Elementos que aparecem nas caixinhas dos inputs
+  document.getElementById("campo1").value = p.name;
+  document.getElementById("campo2").value = p.species;
+  document.getElementById("campo3").value = p.house;
+  document.getElementById("campo4").value = p.actor;
+  document.getElementById("imgPersonagem").src = p.image;
+}
