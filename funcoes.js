808aab514cd245fc3020824920197457984ca5a1
NotFelBRo <tagm554@gmail.com>
2026-05-19 21:31:50 -0300

    Correção das funções a partir da linha 31 até a 55

--- funcoes.js
+++ funcoes.js
@@ -1,48 +1,55 @@
-// Aqui, eu iniciei buscando os dados da lista de persongens do hp-api
-async function buscarPersonagem() {
-  const response = await fetch("https://hp-api.onrender.com/api/characters");
-  const personagens = await response.json();
-  console.log(personagens);
-}
-
-// Aqui, iniciei uma função em que todos os personagens da lista aparecerem no quadro da página e as suas descrição.
-buscarPersonagem();
-
 let personagens = [];
 let indexAtual = 0;
 
-//listei todos os persomagems
+// Aqui na função, vai ser listado todos os personagens listados no link
 async function carregarPersonagens() {
   const response = await fetch("https://hp-api.onrender.com/api/characters");
-  const personagens = await response.json();
+  personagens = await response.json(); // sem "const" para salvar no array externo
 }
 
-// as imagens e descrições aparecem na página
+// Aqui, as informações dos personagens vai aparecer no input (os dados) e a imagem vai aparecer no bloco
 function mostrarPersonagem(index) {
   const p = personagens[index];
   if (!p) return;
 
-  // Elementos que aparecem nas caixinhas dos inputs
   document.getElementById("campo1").value = p.name;
   document.getElementById("campo2").value = p.species;
   document.getElementById("campo3").value = p.house;
   document.getElementById("campo4").value = p.actor;
   document.getElementById("imgPersonagem").src = p.image;
 }
-// O botão tem uma função de click no botão do lado do input de busca, ele vai trazer os elementos dos personagens e esses elementos aparecem nas informações abaixo.
+
+// Adicionado a função de buscar os personagens pelo nome apenas, escrevendo no campo de busca, e o botão vai buscar o nome pela lista e carregar as informações de acordo com os campos(inputs).
 document.getElementById("btnBuscar").addEventListener("click", () => {
   const busca = document.getElementById("campoBusca").value.toLowerCase();
-
+  // caso encontre o personagem
   const encontrado = personagens.findIndex((p) =>
     p.name.toLowerCase().includes(busca),
   );
 
-  // Caso eu digitar o nome do personagem e ele existir, o personagem foi encontrado, caso ele não existir, ele não foi encontrado.
-
-  if (personagens == !-1) {
+  if (encontrado !== -1) {
     indexAtual = encontrado;
-    exibirPersonagem(indexAtual);
+    mostrarPersonagem(indexAtual);
   } else {
-    alert("Personagem não encontrado!");
+    alert("Personagem não encontrado!"); // caso não encontrado
   }
 });
+
+// ação do botão de voltar para o personagem anterior
+document.getElementById("btnVoltar").addEventListener("click", () => {
+  if (indexAtual > 0) {
+    indexAtual--;
+    mostrarPersonagem(indexAtual);
+  }
+});
+
+// o mesmo de cima, porém ele avança para o próximo personagem
+document.getElementById("btnAvancar").addEventListener("click", () => {
+  if (indexAtual < personagens.length - 1) {
+    indexAtual++;
+    mostrarPersonagem(indexAtual);
+  }
+});
+
+// Carrega os personagens
+carregarPersonagens();
