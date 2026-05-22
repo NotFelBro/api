8596387696cc07cb80bd41f644432dc600b302f6
NotFelBRo <tagm554@gmail.com>
2026-05-22 19:09:13 -0300

    adicionei uma função para avisar ao usuário sobre personagens fora da array no html

--- funcoes.js
+++ funcoes.js
@@ -2,25 +2,9 @@ let personagens = [];
 let indexAtual = 0;
 
 async function carregarPersonagens() {
-  try {
-    const response = await fetch("https://hp-api.onrender.com/api/characters");
-
-    if (response.status === 404) {
-      throw new Error("404 - Personagem não encontrado.");
-    }
-    if (response.status === 500) {
-      throw new Error("500 - Falha no servidor.");
-    }
-
-    personagens = await response.json();
-    mostrarPersonagem(indexAtual);
-  } catch (error) {
-    const divErro = document.createElement("div");
-    divErro.textContent = error.message;
-    divErro.style.color = "red";
-    divErro.style.fontWeight = "bold";
-    document.body.appendChild(divErro);
-  }
+  const response = await fetch("https://hp-api.onrender.com/api/characters");
+  personagens = await response.json();
+  mostrarPersonagem(indexAtual);
 }
 
 function mostrarPersonagem(index) {
@@ -33,6 +17,7 @@ function mostrarPersonagem(index) {
   document.getElementById("quadro4").value = p.name;
   document.getElementById("imagePersonagem").src = p.image;
 }
+const inputB = document.getElementById("quadroB");
 
 document.getElementById("btnB").addEventListener("click", () => {
   const busca = document.getElementById("quadroB").value.toLowerCase();
@@ -40,23 +25,20 @@ document.getElementById("btnB").addEventListener("click", () => {
     p.name.toLowerCase().includes(busca),
   );
 
-  const erroExistente = document.getElementById("divErro");
-  if (erroExistente) erroExistente.remove();
-
   if (encontrado !== -1) {
+    inputB.placeholder = "Digite o nome do personagem.";
+    inputB.style.color = "";
     indexAtual = encontrado;
     mostrarPersonagem(indexAtual);
   } else {
-    const erro = new Error("404 - Personagem não econtrado.");
-    console.log(erro.message);
-
-    const divErro = document.createElement("div");
-    divErro.id = "DivErro";
-    divErro = textContent = erro.message;
-    divErro.style.color = "red";
-    divErro.style.fontWeight = "bold";
-    document.body.appendChild(divErro);
+    inputB.value = "";
+    inputB.placeholder = "Personagem inexistente.";
+    inputB.color = "red";
   }
+});
+document.getElementById("quadroB").addEventListener("input", () => {
+  inputB.placeholder = "Digite o nome do personagem";
+  inputB.style.color = "";
 });
 
 document.getElementById("btnV").addEventListener("click", () => {
